04a6cff2951807855806368a4e358312035438c1
QuanliWang <qw2192@Quanlis-MacBook-Pro.local>
2016-02-05 06:41:42 -0500

    use a separate function to preprocess data.

---
+++ Matlab/PrepareData.m
@@ -0,0 +1,20 @@
+function [SS,origdata00] = PrepareData(Household)
+    size234alldata = load('ACShouseholddata_size234all.mat');
+    newdata = size234alldata.newdata;
+
+    HHindex = unique(newdata(:,1)); 
+    SSfull = hist(newdata(:,1),HHindex); 
+
+    randindex = randsample(length(SSfull),Household);
+    SS = SSfull(randindex);
+    
+    origdata00 = [];
+    for i=1:Household
+        index_i = find(newdata(:,1)==randindex(i));
+        base_vec = ones(length(index_i),1);
+        currentHH = [base_vec * i newdata(index_i,2:end) base_vec * SS(i)];
+        origdata00 = [origdata00;currentHH];
+    end
+    
+end
+
