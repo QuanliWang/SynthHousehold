68015d9bda1dbf9f1ed42d58e24d182ad96f8592
QuanliWang <qw2192@Quanlis-MacBook-Pro.local>
2016-02-16 16:47:00 -0500

    Organize all hyper parameters into a hyper object.
    All the fields in hyper object now contains parameters that won’t
    change during MCMC. K and L might need some consideration though. They
    don’t change, but should we place it there?

--- Matlab/init.m
+++ Matlab/init.m
@@ -3,9 +3,6 @@ addpath dist
 %% initilize initial data
 initInputData;
 
-% what are those?
-hyper.dHH = [2 3];
-
 %%
 %%%%---global parameters---%%
 %nrun=10000; burn=8000; thin=10; %50;
@@ -13,49 +10,53 @@ hyper.dHH = [2 3];
 nrun=25; burn=20; thin=1;
 eff.sam=(nrun-burn)/thin;
 
-K=40;                     
-L=15;
-aa=0.25; ab=0.25;                  % gamma hyperparameters for alpha
-ba=0.25; bb=0.25;
+
+% what are those?
+hyper.dHH = [2 3];
+
+hyper.K=40;                     
+hyper.L=15;
+hyper.aa=0.25; hyper.ab=0.25;                  % gamma hyperparameters for alpha
+hyper.ba=0.25; hyper.bb=0.25;
 
 %%%%---initial values---%%
 alpha=1;        % hyperparameters for stick-breaking weights
 beta=1;
 
-phi=zeros(orig.maxd,orig.p,K*L);      % cell probabilities
+phi=zeros(orig.maxd,orig.p,hyper.K*hyper.L);      % cell probabilities
 for j=1:orig.p
     for c=1:orig.d(j)
         phi(c,j,1)=sum(orig.data(:,j)==c)/orig.n_individuals;
     end
 end
-for count = 2:K*L
+for count = 2:hyper.K*hyper.L
     phi(:,:,count) = phi(:,:,1);
 end
 
 HHdata_all = orig.HHdataorig; HHdata_all(:,2) = HHdata_all(:,2) - 1;
 
 %initialize lambda
-lambda1 = zeros(K,dHH(1));
-for c=1:dHH(1)
+lambda1 = zeros(hyper.K,hyper.dHH(1));
+for c=1:hyper.dHH(1)
     lambda1(:,c) = sum(HHdata_all(:,1)==c)/orig.n;
 end
 
-lambda2 = zeros(K,dHH(2));
-for c=1:dHH(2)
+lambda2 = zeros(hyper.K,hyper.dHH(2));
+for c=1:hyper.dHH(2)
     lambda2(:,c) = sum(HHdata_all(:,2)==c)/orig.n;
 end
 
-u=[betarnd(1,alpha,[K-1,1]);1];
-pi=zeros(K,1);
-pi(1:K)=u.*cumprod([1;1-u(1:K-1)]);
+u=[betarnd(1,alpha,[hyper.K-1,1]);1];
+pi=zeros(hyper.K,1);
+pi(1:hyper.K)=u.*cumprod([1;1-u(1:hyper.K-1)]);
 
-v=[betarnd(1,beta,[K,L-1]),ones(K,1)];
-w=zeros(K,L);
+v=[betarnd(1,beta,[hyper.K,hyper.L-1]),ones(hyper.K,1)];
+w=zeros(hyper.K,hyper.L);
 v1 = (v(1,:))';
-w(1,1:L)=v1.*cumprod([1;1-v1(1:L-1)]);
-for k=1:K
+w(1,1:hyper.L)=v1.*cumprod([1;1-v1(1:hyper.L-1)]);
+for k=1:hyper.K
     v1 = (v(k,:))';
-    w(k,1:L)=v1.*cumprod([1;1-v1(1:L-1)]);
+    w(k,1:hyper.L)=v1.*cumprod([1;1-v1(1:hyper.L-1)]);
 end
 
 %
@@ -67,8 +68,8 @@ ImpossibleIndividuals = [];z_HH_extra = [];
 %%%%---define output files---%%
 alphaout = zeros(nrun,1); 
 betaout = zeros(nrun,1);
-piout = zeros(eff.sam,K); 
-wout = zeros(eff.sam,K,L);
+piout = zeros(eff.sam,hyper.K); 
+wout = zeros(eff.sam,hyper.K,hyper.L);
 nout = zeros(nrun,1);
 n_sout = zeros(nrun,1);
 
@@ -78,6 +79,6 @@ z_HH_save = zeros(nrun,orig.n_individuals);
 z_member_save = zeros(nrun,orig.n_individuals);
 elapsed_time = zeros(1,nrun);
 
-newphiout = zeros(eff.sam,orig.maxd*orig.p,K*L);
-lambda1out = zeros(eff.sam,K,dHH(1));
-lambda2out = zeros(eff.sam,K,dHH(2));
+newphiout = zeros(eff.sam,orig.maxd*orig.p,hyper.K*hyper.L);
+lambda1out = zeros(eff.sam,hyper.K,hyper.dHH(1));
+lambda2out = zeros(eff.sam,hyper.K,hyper.dHH(2));
--- Matlab/master.m
+++ Matlab/master.m
@@ -8,19 +8,19 @@ for i = 1:nrun
     
     %% update zHH
     [z_HH_all, z_HH_Individuals_all,newphi] = UpdateHouseholdIndicator(...
-        orig.n,orig.n_individuals,orig.maxd,orig.p, K,L,...
+        orig.n,orig.n_individuals,orig.maxd,orig.p, hyper.K,hyper.L,...
     phi,orig.dataT,orig.SS,w,pi,HHdata_all,lambda1,lambda2,ImpossibleIndividuals,z_HH_extra);
 
     %% update zmember
     z_Individual_all = UpdateIndividualIndicator(orig.n,orig.n_individuals,...
-    newphi,orig.dataT,w,K,L,orig.p,orig.maxd,z_HH_all,orig.HHserial,ImpossibleIndividuals);
+    newphi,orig.dataT,w,hyper.K,hyper.L,orig.p,orig.maxd,z_HH_all,orig.HHserial,ImpossibleIndividuals);
 
     %% update phi
-    [phi,phicountcluster,kcount] = UpdatePhi(data_full_all,K,L,orig.p,orig.d,orig.maxd,...
+    [phi,phicountcluster,kcount] = UpdatePhi(data_full_all,hyper.K,hyper.L,orig.p,orig.d,orig.maxd,...
         z_HH_Individuals_all,z_Individual_all,z_HH_all);
     
     %% update lambda
-    [lambda1,lambda2] = UpdateLambda(dHH,K,z_HH_all,HHdata_all);
+    [lambda1,lambda2] = UpdateLambda(hyper.dHH,hyper.K,z_HH_all,HHdata_all);
     
     %% update pi
     [pi,u] = UpdatePi(alpha,kcount);
@@ -29,14 +29,14 @@ for i = 1:nrun
     [w,v] = UpdateW(beta,phicountcluster);
      
     %% update alpha
-    alpha = UpdateAlpha(aa,ab,K,u);
+    alpha = UpdateAlpha(hyper.aa,hyper.ab,u);
     
     %% update alpha
-    beta = UpdateBeta(ba,bb,v);
+    beta = UpdateBeta(hyper.ba,hyper.bb,v);
     
     %% generate impossible house hold
     [hh_size_new,ImpossibleIndividuals,data_full_all,z_HH_extra,HHdata_all] = ...
-    GetImpossibleHouseholds(lambda1,lambda2,w,phi,orig.d,orig.p,L, orig.ACS_count,pi,...
+    GetImpossibleHouseholds(lambda1,lambda2,w,phi,orig.d,orig.p,hyper.L, orig.ACS_count,pi,...
     orig.origdata,orig.HHdataorig);
    
     postsave;
