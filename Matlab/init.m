89d809ee8b048dc3a36e4be2a9d0d78b776c028d
QuanliWang <qw2192@Quanlis-MacBook-Pro.local>
2016-02-16 16:28:42 -0500

    move input initialization into one script.

--- Matlab/init.m
+++ Matlab/init.m
@@ -1,46 +1,13 @@
 addpath dist
 
-if exist('household.mat', 'file') == 2
-    disp('using existing data')
-    load household
-else
-    disp('random sampling new data')
-    n = 10000;
-    [SS,origdata] = PrepareData('Monica/ACShouseholddata_size234all.mat',n);
-    save household
-    %dlmwrite('data.txt',origdata,'delimiter','\t');
-end
+%% initilize initial data
+initInputData;
 
-%%
-orig.origdata = origdata;
-orig.SS = SS;
-orig.n = length(orig.SS);
-HHrowIndex = [1 cumsum(orig.SS)+1]; 
-orig.HHdataorig = orig.origdata(HHrowIndex(1: (end -1)),8:9);
-HHdata_all = orig.HHdataorig; HHdata_all(:,2) = HHdata_all(:,2) - 1;
-orig.HHserial = orig.origdata(:,1);
-clear origdata SS n desc HHrowIndex
-
-[orig.n_individuals,orig.p] = size(orig.origdata);
-orig.p = orig.p-4;
-
-% variables: HHindex 1, pernum 2, sex 3, race 4, ethn 5, age 6, relate
-% 7(ind level) ownership 8 (binary, HH level) household size 
-orig.d = [2,9,5,94,12]; %number of levels for each variable (5 variables all together)
-orig.data = orig.origdata(:,3:7);
-orig.dataT = orig.origdata(:,3:7)';
-orig.maxd = max(orig.d);
-
-orig.ACS_count = zeros(3,1);
-orig.ACS_count(1) = sum(orig.SS==2);
-orig.ACS_count(2) = sum(orig.SS==3);
-orig.ACS_count(3) = sum(orig.SS==4);
 % what are those?
-dHH = [2 3];
+hyper.dHH = [2 3];
 
 %%
 %%%%---global parameters---%%
-
 %nrun=10000; burn=8000; thin=10; %50;
 %nrun=1000; burn=800; thin=10; %50;
 nrun=25; burn=20; thin=1;
@@ -50,24 +17,6 @@ K=40;
 L=15;
 aa=0.25; ab=0.25;                  % gamma hyperparameters for alpha
 ba=0.25; bb=0.25;
-
-%%%%---define output files---%%
-alphaout = zeros(nrun,1); 
-betaout = zeros(nrun,1);
-piout = zeros(eff.sam,K); 
-wout = zeros(eff.sam,K,L);
-nout = zeros(nrun,1);
-n_sout = zeros(nrun,1);
-
-extrasize = zeros(nrun,3);
-
-z_HH_save = zeros(nrun,orig.n_individuals);
-z_member_save = zeros(nrun,orig.n_individuals);
-elapsed_time = zeros(1,nrun);
-
-newphiout = zeros(eff.sam,orig.maxd*orig.p,K*L);
-lambda1out = zeros(eff.sam,K,dHH(1));
-lambda2out = zeros(eff.sam,K,dHH(2));
 
 %%%%---initial values---%%
 alpha=1;        % hyperparameters for stick-breaking weights
@@ -82,6 +31,8 @@ end
 for count = 2:K*L
     phi(:,:,count) = phi(:,:,1);
 end
+
+HHdata_all = orig.HHdataorig; HHdata_all(:,2) = HHdata_all(:,2) - 1;
 
 %initialize lambda
 lambda1 = zeros(K,dHH(1));
@@ -111,3 +62,22 @@ end
 data_full_all = orig.origdata;
 
 ImpossibleIndividuals = [];z_HH_extra = [];
+
+
+%%%%---define output files---%%
+alphaout = zeros(nrun,1); 
+betaout = zeros(nrun,1);
+piout = zeros(eff.sam,K); 
+wout = zeros(eff.sam,K,L);
+nout = zeros(nrun,1);
+n_sout = zeros(nrun,1);
+
+extrasize = zeros(nrun,3);
+
+z_HH_save = zeros(nrun,orig.n_individuals);
+z_member_save = zeros(nrun,orig.n_individuals);
+elapsed_time = zeros(1,nrun);
+
+newphiout = zeros(eff.sam,orig.maxd*orig.p,K*L);
+lambda1out = zeros(eff.sam,K,dHH(1));
+lambda2out = zeros(eff.sam,K,dHH(2));
