375b08fdd98b8b1dbd8d6256157f44640791f0c4
QuanliWang <qw2192@Quanlis-MacBook-Pro.local>
2016-02-19 15:13:33 -0500

    Clean up

--- Matlab/GenerateData2Check_all.m
+++ Matlab/GenerateData2Check_all.m
@@ -1,6 +1,9 @@
-function [data_to_check_all,outcome_to_save, number_of_generation] = ...
+function [Individuals,HouseHolds, number_of_generation] = ...
     GenerateData2Check_all(hh_size,lambda, w, ...
     phi,pi, d, p, cum_number_of_generation,L,ACS_count,newxtras)
+    
+    %Individuals_all = [];
+    %HouseHolds_all = [];
     data_to_check_all = zeros(newxtras,8*hh_size+1+hh_size);
     outcome_to_save = zeros(newxtras,1);
     number_of_generation = 0;
@@ -23,4 +26,26 @@ function [data_to_check_all,outcome_to_save, number_of_generation] = ...
     end
     
     number_of_generation = number_of_generation + cum_number_of_generation;
+    
+    %impossible household
+    extras_old2 = data_to_check_all(outcome_to_save==0 &...
+        data_to_check_all(:,1) > 0,:);
+
+    %possible household
+    possiblehh = data_to_check_all(outcome_to_save==1,1);
+    
+    %find the number ACS_count(hh_size-1)'th possible household 
+    exceeding_index = possiblehh(ACS_count(hh_size-1),1);
+    
+    %Get all the impossible households we need
+    HouseHolds = extras_old2(extras_old2(:,1) < exceeding_index ...
+        & extras_old2(:,1) >0,:);
+     
+    t1 = HouseHolds(:,1:hh_size * 8)';
+    t2 = HouseHolds(:,hh_size * 8 +1)';
+    t3 = HouseHolds(:,hh_size * 8 +1 + (1:hh_size))';
+    
+    Individuals = [reshape(t1,8,[])' reshape(repmat(t2,hh_size,1),[],1)...
+        reshape(t3,[],1)];
+    
 end
