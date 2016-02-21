bc26a1b71132a2b363373e0effb4969e4cb7159a
QuanliWang <qw2192@Quanlis-MacBook-Pro.local>
2016-02-21 18:30:29 -0500

    optimize UpdatePhi and UpdateW

--- Matlab/UpdateW.m
+++ Matlab/UpdateW.m
@@ -1,18 +1,13 @@
 function [w,v] = UpdateW(beta,z_Individual_all, K, L)
+    
     phicountcluster = groupcount(z_Individual_all(:,1),z_Individual_all(:,2),K,L);
-    v = zeros(K,L);
-    w=zeros(K,L);
-    for k = 1:K
-        vk = v(k,:);
-        for l = 1:L-1
-            vk(l) = betarnd(1 + phicountcluster(k,l), beta + sum(phicountcluster(k,l+1:L)));
-            if vk(l)>1-1e-5
-                vk(l)=1-1e-5;
-            end
-            vk(L) = 1;
-        end
-        v(k,:) = vk;
-        w(k,1:L) = vk.*cumprod([1,1-vk(1:L-1)]);
-    end
+    cum = fliplr(cumsum(fliplr(phicountcluster),2));
+    
+    v = betarnd(1 + phicountcluster(:,1:(L-1)),beta + cum(:,2:L));
+    v(v > 1-1e-5) = 1 - 1-1e-5;
+    v = [v  ones(K,1)];
+    
+    w = v.* cumprod([ones(K,1) 1 - v(:,1:(L-1))],2);
+
 end
 
