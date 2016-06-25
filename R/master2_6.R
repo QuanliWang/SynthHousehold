8018f87eda1677c048eaf30a6619cbabf808f171
QuanliWang <qw2192@Quanlis-MacBook-Pro.local>
2016-06-25 13:05:30 -0400

    generalize househols level input data

--- R/master2_6.R
+++ R/master2_6.R
@@ -1,7 +1,7 @@
 rm(list = ls())
 library(NestedCategBayesImpute)
 library(dplyr)
-orig.file <- system.file("extdata", "origdata.txt", package = "NestedCategBayesImpute")
+orig.file <- system.file("extdata", "origdata_new.txt", package = "NestedCategBayesImpute")
 orig.data <- read.table(orig.file,header = TRUE, sep = " ")
 names(orig.data) <- c("Hhindex","pernum", "sex", "race", "sthn","age","relate","ownership",
                       "headsex", "headrace", "headsthn","headage","headrelate")
@@ -9,18 +9,27 @@ household.size <- as.data.frame(table(orig.data[,1]))
 household.size[,1] <- as.numeric(household.size[,1])
 names(household.size) <- c("Hhindex", 'householdsize')
 household <- orig.data %>% inner_join(household.size)
-orig <- initData(household)
-orig$HHdataorigT <- orig$HHdataorigT + 1 ## Michael
+
+individual_varible_index = c(3:7)
+
+#column 8 to 14 are household level data
+household_variable_index = c(8,14,9:13)
+
+orig <- initData(household, individual_varible_index,household_variable_index)
+#orig$HHdataorigT <- orig$HHdataorigT + 1 ## Michael
 
 #mcmc parameters
 mc <- list(nrun = 10000, burn = 5000, thin = 50)
 mc$eff.sam <- (mc$nrun-mc$burn)/mc$thin
 #hyper parameters
 #aa ab: gamma hyperparameters for alpha
-#hyper <- list(K=40 , L=15, aa=0.25, ab=0.25, ba=0.25,bb=0.25,
-#              dHH = c(2,length(unique(household[,"householdsize"]))), blocksize = 10000)
+
+dHH <- rep(0,length(household_variable_index))
+for (i in 1:length(dHH)) {
+  dHH[i] <- max(household[,household_variable_index[i]])
+}
 hyper <- list(K=40 , L=15, aa=0.25, ab=0.25, ba=0.25,bb=0.25,
-              dHH = c(orig$d,2,length(unique(household[,"householdsize"]))), blocksize = 10000)
+              dHH = dHH, blocksize = 10000)
 para <- initParameters(orig,hyper)
 output <- initOutput(orig,hyper,mc)
 
