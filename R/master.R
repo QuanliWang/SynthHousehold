818a9915c9716e943282901d2415d5ca284ffc9e
Olanrewaju Akande <Olanrewaju@Olanrewajus-MacBook-Pro.local>
2016-09-22 19:53:32 -0400

    Add option to implement different capping/weighting option for each household size

--- R/master.R
+++ R/master.R
@@ -1,3 +1,5 @@
+
+
 rm(list = ls())
 library(NestedCategBayesImpute)
 library(dplyr)
@@ -18,7 +20,15 @@ if (format2) {
   household_variable_index = c(8:13) #column 8 to 13 are household level data #make sure the last one is household size
 } else {
   #set up data set
-  data(household)
+  #data(household)
+  orig.file <- system.file("extdata", "origdata_oldFormat.txt", package = "NestedCategBayesImpute")
+  orig.data <- read.table(orig.file,header = TRUE, sep = " ")
+  names(orig.data) <- c("Hhindex","pernum", "sex", "race", "sthn","age","relate","ownership")
+  household.size <- as.data.frame(table(orig.data[,1]))
+  household.size[,1] <- as.numeric(household.size[,1])
+  names(household.size) <- c("Hhindex", 'householdsize')
+  household <- orig.data %>% inner_join(household.size)
+  
   individual_varible_index = c(3:7)
   household_variable_index = c(8,9) #make sure the last one is household size
 }
@@ -45,8 +55,43 @@ hyper <- list(K=40 , L=15, aa=0.25, ab=0.25, ba=0.25,bb=0.25,
 para <- initParameters(orig,hyper,format2)
 output <- initOutput(orig,hyper,mc)
 
-synindex <- c(9910,9920,9930,9940,9950,9960,9970,9980,9990,10000)
+#synindex <- c(9910,9920,9930,9940,9950,9960,9970,9980,9990,10000)
+synindex <- sort(sample(seq((mc$nrun*0.8 +1),mc$nrun,mc$eff.sam),10,replace=F))
 synData <- list()
 
-source("../mcmc.R")
+#weighting
+struc_weight <- c(1,1,1,1/2,1/3) # must be ordered & no household size must be excluded
+struc_weight <- c(1,struc_weight) #add 1 for the weight of the observed data
+weighted_z_HH_all <- vector("list",length(struc_weight))
+weighted_HHdata_all <- vector("list",length(struc_weight))
+weighted_IndividualData_all <- vector("list",length(struc_weight))
+weighted_z_Individual_all <- vector("list",length(struc_weight))
+
+
+#source("../mcmc.R")
+source("mcmc.R")
+
+
+#save synthetic data
+writeFun <- function(LL){
+  for(i in 1:length(LL)){
+    if(format2){
+      if(length(which(struc_weight!=1))>0){
+        write.table(t(LL[[i]]),paste0("synData_newFormat_weighted",i,".txt"),row.names = F,col.names = F)
+      } else {
+        write.table(t(LL[[i]]),paste0("synData_newFormat",i,".txt"),row.names = F,col.names = F)
+      }
+    } else {
+      if(length(which(struc_weight!=1))>0){
+        write.table(t(LL[[i]]),paste0("synData_oldFormat_weighted",i,".txt"),row.names = F,col.names = F)
+      } else {
+        write.table(t(LL[[i]]),paste0("synData_oldFormat",i,".txt"),row.names = F,col.names = F)
+      }
+    }
+  }
+}
+writeFun(synData)
+
+
+
 
--- R/mcmc.R
+++ R/mcmc.R
@@ -5,15 +5,19 @@ for (i in 1:mc$nrun) {
   z_household <- samplezHH(para$phi,orig$dataT,para$w,para$pi,orig$SS,t(para$HHdata_all[,1:orig$n]),para$lambda)
 
   z_Individuals <- samplezmember(para$phi,orig$dataT,para$w,z_household$z_HH,orig$HHserial)
-  data.extra <- GetImpossibleHouseholds(orig$d,orig$ACS_count,para$lambda,para$w,para$phi,
+  
+  data.extra <- GetImpossibleHouseholds(orig$d,ceiling(orig$ACS_count*struc_weight[-1]),para$lambda,para$w,para$phi,
                                         para$pi,hyper$blocksize,orig$n,is.element(i,synindex),format2)
   para$hh_size_new <- as.vector(data.extra$hh_size_new)
-
   DIM <- dim(data.extra$IndividualData_extra)[1]
+  
   if (is.element(i,synindex)) {
-    synData[[which(synindex ==i)]] <- data.extra$synIndividuals_all[1:DIM,]
+    forsynData <- GetImpossibleHouseholds(orig$d,orig$ACS_count,para$lambda,para$w,para$phi,
+                                          para$pi,hyper$blocksize,orig$n,is.element(i,synindex),format2) #synthetic data
+    synData[[which(synindex ==i)]] <- forsynData$synIndividuals_all[1:dim(forsynData$IndividualData_extra)[1],]
   }
-
+  
+  
   #combine data and indicators
   para$z_HH_all <- c(z_household$z_HH, data.extra$z_HH_extra)
   para$HHdata_all <- orig$HHdataorigT
@@ -26,21 +30,47 @@ for (i in 1:mc$nrun) {
   #row 1 for K groups and row 2 for L groups
   temp <- rbind(z_household$z_HH_Individuals,z_Individuals)
   para$z_Individual_all  <- cbind(temp,data.extra$z_HHdata_individual_extra)
+  
+  
+  #re-combine data and indicators into lists for weighting
+  n_i <- as.data.frame(table(data.extra$IndividualData_extra[1,]))$Freq
+  n_i_index <- rep(n_i,n_i)
+  weighted_z_HH_all[[1]] <- z_household$z_HH
+  temp_temp <- orig$HHdataorigT
+  if (!format2) {
+    temp_temp[2,] <- temp_temp[2,] -1
+  }
+  weighted_HHdata_all[[1]] <- temp_temp
+  weighted_IndividualData_all[[1]] <- t(orig$origdata[,1:DIM])
+  weighted_z_Individual_all[[1]] <- rbind(z_household$z_HH_Individuals,z_Individuals)
+  
+  for(w_i in 2:length(struc_weight)){
+    weighted_z_HH_all[[w_i]] <- data.extra$z_HH_extra[which(n_i == (w_i-1))]
+    weighted_HHdata_all[[w_i]] <- data.extra$HHdata_extra[,which(n_i == (w_i-1))]
+    weighted_IndividualData_all[[w_i]] <- data.extra$IndividualData_extra[,which(n_i_index == (w_i-1))]
+    weighted_z_Individual_all[[w_i]] <- data.extra$z_HHdata_individual_extra[,which(n_i_index == (w_i-1))]
+  }
+  
 
   # update phi
-  para$phi <- UpdatePhi(para$IndividualData_all,para$z_Individual_all,
-                        hyper$K,hyper$L,orig$p,orig$d,orig$maxd,individual_varible_index)
+  #para$phi <- UpdatePhi(para$IndividualData_all,para$z_Individual_all,
+  #                      hyper$K,hyper$L,orig$p,orig$d,orig$maxd,individual_varible_index)
+  para$phi <- UpdatePhiWeighted(weighted_IndividualData_all,weighted_z_Individual_all,
+                        hyper$K,hyper$L,orig$p,orig$d,orig$maxd,individual_varible_index,struc_weight)
 
   #update W
-  W <- UpdateW(para$beta,para$z_Individual_all, hyper$K, hyper$L)
+  #W <- UpdateW(para$beta,para$z_Individual_all, hyper$K, hyper$L)
+  W <- UpdateWWeighted(para$beta,weighted_z_Individual_all, hyper$K, hyper$L,struc_weight)
   para$w <- W$w
   para$v <- W$v
 
   # update lambda
-  para$lambda <- UpdateLambda(hyper$dHH,hyper$K,para$z_HH_all,para$HHdata_all)
+  #para$lambda <- UpdateLambda(hyper$dHH,hyper$K,para$z_HH_all,para$HHdata_all)
+  para$lambda <- UpdateLambdaWeighted(hyper$dHH,hyper$K,weighted_z_HH_all,weighted_HHdata_all,struc_weight)
 
   # update pi
-  Pi <- UpdatePi(para$alpha,para$z_HH_all,hyper$K)
+  #Pi <- UpdatePi(para$alpha,para$z_HH_all,hyper$K)
+  Pi <- UpdatePiWeighted(para$alpha,para$z_HH_all,hyper$K,struc_weight)
   para$pi <- Pi$pi
   para$u <- Pi$u
 
@@ -62,7 +92,9 @@ for (i in 1:mc$nrun) {
   }
 
   total_household <- dim(para$HHdata_all)[2]
-  cat(paste("number of household is ", total_household, "\n", sep = ''))
+  cat(paste("sampled number of households is ", total_household, "\n", sep = ''))
+  est_total_household <- sum(c(orig$n,para$hh_size_new)/struc_weight)
+  cat(paste("estimated (true) number of households is ", est_total_household, "\n", sep = ''))
 
   output$elapsed_time[i] <- (proc.time() - t)[["elapsed"]]
   cat(paste("elapsed time = ", output$elapsed_time[i], "\n\n", sep = ' '))
