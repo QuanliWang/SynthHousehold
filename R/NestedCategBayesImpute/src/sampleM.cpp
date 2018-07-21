15934e3d0ed90c8a38e8df391246d0a2ad669fbc
QuanliWang <qw2192@Quanlis-MacBook-Pro.local>
2018-07-20 22:37:23 -0400

    code clean up

--- R/NestedCategBayesImpute/src/sampleM.cpp
+++ R/NestedCategBayesImpute/src/sampleM.cpp
@@ -6,40 +6,35 @@ using namespace Rcpp;
 // [[Rcpp::export]]
 NumericVector sampleM(NumericMatrix phi, IntegerMatrix data,
                NumericMatrix omega, IntegerVector G, IntegerVector serial) {
-
-  //printf("in sampleM\n");
   int p = data.nrow();
   int nIndividuals = data.ncol();
   int FF = omega.nrow();
   int SS = omega.ncol();
   int maxDDtp = phi.nrow();
   int maxdd = maxDDtp / p;
-  //printf("maxdd = %d\n", maxdd);
   NumericVector indi(nIndividuals);
   NumericVector rand = runif(nIndividuals);
-
   double *Gupdateprob2= new double[SS];
   for (int m = 0; m < nIndividuals; m++) {
 
     int G_asg = G[int(serial[m])-1];
     int base = m*p;
+    int base2 = maxDDtp*((G_asg-1)*SS);
     for (int l = 0; l < SS; l++) {
       try {
         double phiprod = 1.0;
         for (int j = 0; j < p; j++) {
           int u = data[base+j]-1;
-          phiprod *= phi[maxDDtp*((G_asg-1)*SS+l)+j*maxdd+u];
+          phiprod *= phi[base2+j*maxdd+u];
         }
-        Gupdateprob2[l] = omega[FF*l+G_asg-1]*phiprod;
+        Gupdateprob2[l] = omega[FF*l+G_asg-1]*phiprod; //can use logrithm to speed up here and also for accuracy
       } catch(...) {
         Gupdateprob2[l] = 0;
       }
     }
     indi[m] = samplew(Gupdateprob2, SS, rand[m]);
-    //printf("indi = (%d,%d)\n",m, (int)indi[m]);
   }
   delete [] Gupdateprob2;
-  //printf("done sampleM\n");
   return indi;
 }
 
