42b88530d95796fe0ad2f787a6c5406cc551a497
QuanliWang <qw2192@Quanlis-MacBook-Pro.local>
2018-07-21 21:12:51 -0400

    add rbenchmark

--- R/master.R
+++ R/master.R
@@ -7,7 +7,7 @@ library(dplyr)
 ### Also set indicator for the weighting/capping option
 options <- list()
 options$HHhead_at_group_level <- TRUE #set to TRUE to move household head to the group level
-options$weight_option <- FALSE #set to FALSE for weighting/capping option. If TRUE, must supply weights
+options$weight_option <- TRUE #set to FALSE for weighting/capping option. If TRUE, must supply weights
 
 source("../GetExampleData.R")
 
@@ -67,15 +67,30 @@ MissData$miss_index <- sort(sample(seq((mc$burn +1),mc$nrun,by=mc$thin),mm,repla
 
 ### Run model
 proc_t <- proc.time()
-mc <- list(nrun = 200, burn = 50, thin = 5)
+#mc <- list(nrun = 200, burn = 50, thin = 5)
 #Rprof()
 library(profvis)
-profvis({
+library(rbenchmark)
+#profvis({
+benchmark("serial" = {ModelResults <- RunModel(orig,mc,hyper,para,output,synindex,
+                                                ExampleData$individual_variable_index,
+                                                ExampleData$household_variable_index,
+                                                options$HHhead_at_group_level,options$weight_option,struc_weight,MissData, Parallel = FALSE)},
+           "parallel" = {ModelResults <- RunModel(orig,mc,hyper,para,output,synindex,
+                                                  ExampleData$individual_variable_index,
+                                                  ExampleData$household_variable_index,
+                                                  options$HHhead_at_group_level,options$weight_option,struc_weight,MissData, Parallel = TRUE)},
+          replications = 5,
+          columns = c("test", "replications", "elapsed",
+                      "relative", "user.self", "sys.self"))
+
+
+
 ModelResults <- RunModel(orig,mc,hyper,para,output,synindex,
                          ExampleData$individual_variable_index,
                          ExampleData$household_variable_index,
-                         options$HHhead_at_group_level,options$weight_option,struc_weight,MissData)
-})
+                         options$HHhead_at_group_level,options$weight_option,struc_weight,MissData, Parallel = TRUE)
+#})
 
 #Rprof(NULL)
 total_time <- (proc.time() - proc_t)[["elapsed"]]
@@ -83,15 +98,15 @@ total_time
 
 #summaryRprof()
 
-proc_t <- proc.time()
-#profvis({
-  ModelResults <- RunModel(orig,mc,hyper,para,output,synindex,
-                           ExampleData$individual_variable_index,
-                           ExampleData$household_variable_index,
-                           options$HHhead_at_group_level,options$weight_option,struc_weight,MissData,Parallel = TRUE)
-#})
-total_time <- (proc.time() - proc_t)[["elapsed"]]
-total_time
+#proc_t <- proc.time()
+##profvis({
+#  ModelResults <- RunModel(orig,mc,hyper,para,output,synindex,
+#                           ExampleData$individual_variable_index,
+#                           ExampleData$household_variable_index,
+#                           options$HHhead_at_group_level,options$weight_option,struc_weight,MissData,Parallel = FALSE)
+##})
+#total_time <- (proc.time() - proc_t)[["elapsed"]]
+#total_time
 
 
 ### View first few lines of the first synthetic data.
